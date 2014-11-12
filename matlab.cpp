cc09f2ebfcf846745b6d6986083b862ceaaa20a9
Owen McAree <o.mcaree@sheffield.ac.uk>
2014-11-12 11:31:30 +0000

    Changed UDP tx/rx structure + bugfixes
    Took the UDP tx/rx out of the drive callback because it wasn't syncing
    well with Simulink. Instead, we poll all sockets each time drive is
    executed, any data is then read in to local storage and this is used to
    updated each vehicle.
    
    Also fixed the output of engine RPM (it was outputting rad/s)

--- matlab.cpp
+++ matlab.cpp
@@ -47,6 +47,11 @@ static void endRace(int index, tCarElt *car, tSituation *s);
 
 SOCKET sock[NBBOTS];
 WSADATA wsa;
+pollfd fds[NBBOTS];
+
+struct sockaddr_in client[NBBOTS];
+float throttle[NBBOTS], brake[NBBOTS], clutch[NBBOTS], steering[NBBOTS];
+int gear[NBBOTS];
 
 
 // Module entry point.
@@ -105,66 +110,90 @@ static void newRace(int index, tCarElt* car, tSituation *sit)
 {     
 	struct sockaddr_in server;
 
-    //Create a socket
+    // Create a socket
 	sock[index] = socket(AF_INET, SOCK_DGRAM, 0 );
      
-    //Prepare the sockaddr_in structure
+    // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(BASE_PORT + index);
     
+	// Set tx and rx buffers so we only retain one frame
 	int buf = 20;
 	setsockopt(sock[index],SOL_SOCKET,SO_RCVBUF,(const char*)&buf,sizeof(int));
+	buf = 24;
+	setsockopt(sock[index],SOL_SOCKET,SO_SNDBUF,(const char*)&buf,sizeof(int));
 
-    //Bind
+    // Bind to port
     bind(sock[index],(struct sockaddr *)&server, sizeof(server));
+
+	// Update poll structure
+	fds[index].fd = sock[index];
+	fds[index].events = POLLIN;
+	fds[index].revents = -1;
+
+	printf("matlab %d: Bound to %d (fd %d)\n",index,BASE_PORT + index, sock[index]);
+}
+
+static void udpRXTX(int index, tCarElt* car) {
+	int slen = sizeof(client[index]);
+	char recvBuf[20];
+
+	// Read any pending data (all vehicles) and save to local storage
+	if (WSAPoll(fds,NBBOTS,0)) {
+		for (int i = 0; i<NBBOTS; i++) {
+			if (fds[i].revents & POLLIN) {		
+//				printf("Data for #%d: ", i);
+				int len = recvfrom(fds[i].fd, recvBuf, 20, 0, (struct sockaddr *) &client[i], &slen);
+				if (len == 20) {	// Ignore the spurious data we get on end of simulation
+					throttle[i] = *(float*)(recvBuf);
+					brake[i] = *(float*)(recvBuf+4);
+					clutch[i] = *(float*)(recvBuf+8);
+					gear[i] = *(int*)(recvBuf+12);
+					steering[i] = *(float*)(recvBuf+16);
+				}
+//				printf("%f %f %f %d %f",throttle[i],brake[i],clutch[i],gear[i],steering[i]);
+			}
+		}
+//		printf("\n");
+	}
+	// Update current vehicle from local storage
+	car->ctrl.accelCmd = throttle[index];
+	car->ctrl.brakeCmd = brake[index];
+	car->ctrl.clutchCmd = clutch[index];
+	car->ctrl.gear = gear[index];
+	car->ctrl.steer = steering[index];
+
+	// Output current vehicle to client (if we have one)
+	if (true) {		// TODO: Figure out how to test if client structure is empty
+		float dist = RtGetDistFromStart(car);
+		float speed = car->_speed_x;
+		float angle = RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
+		angle = fmod(angle + 2*PI,2*PI);	// Normalise heading angle
+		if (angle > PI) {
+			angle = angle - 2*PI;
+		}
+		float latErr = car->_trkPos.toMiddle;
+		float radius = car->_trkPos.seg->radius;
+		float rpm = car->_enginerpm * 9.54929659; // For some reason RPM is actually rad/s!
+
+		char sendBuf[24];
+		memcpy(sendBuf,&dist,4);
+		memcpy(sendBuf+4,&speed,4);
+		memcpy(sendBuf+8,&angle,4);
+		memcpy(sendBuf+12,&latErr,4);
+		memcpy(sendBuf+16,&radius,4);
+		memcpy(sendBuf+20,&rpm,4);
+
+		sendto(sock[index],sendBuf,24,0,(const sockaddr *)&client[index],slen);
+	}
 }
 
 
 // Drive during race.
 static void drive(int index, tCarElt* car, tSituation *s)
 {
-	struct sockaddr_in si_other;
-	int slen = sizeof(si_other);
-	pollfd fds;
-	fds.fd = sock[index];
-	fds.events = POLLIN;
-	fds.revents = -1;
-
-	if (WSAPoll(&fds,1,0)) {
-		if (fds.revents | POLLIN) {
-			char recvBuf[20];
-			recvfrom(sock[index], recvBuf, 20, 0, (struct sockaddr *) &si_other, &slen);
-			car->ctrl.accelCmd = *(float*)(recvBuf);
-			car->ctrl.brakeCmd = *(float*)(recvBuf+4);
-			car->ctrl.clutchCmd = *(float*)(recvBuf+8);
-			car->ctrl.gear = *(int*)(recvBuf+12);
-			car->ctrl.steer = *(float*)(recvBuf+16);
-
-			float dist = RtGetDistFromStart(car);
-			float speed = car->_speed_x;
-			float angle = RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
-			angle = fmod(angle + 2*PI,2*PI);
-			if (angle > PI) {
-				angle = angle - 2*PI;
-			}
-			float latErr = car->_trkPos.toMiddle;
-			float radius = car->_trkPos.seg->radius;
-			float rpm = car->_enginerpm;
-
-			char sendBuf[24];
-			memcpy(sendBuf,&dist,4);
-			memcpy(sendBuf+4,&speed,4);
-			memcpy(sendBuf+8,&angle,4);
-			memcpy(sendBuf+12,&latErr,4);
-			memcpy(sendBuf+16,&radius,4);
-			memcpy(sendBuf+20,&rpm,4);
-		
-
-
-			sendto(sock[index],sendBuf,24,0,(const sockaddr *)&si_other,slen);
-		}
-	}
+	udpRXTX(index, car);
 }
 
 
@@ -172,6 +201,7 @@ static void drive(int index, tCarElt* car, tSituation *s)
 static void endRace(int index, tCarElt *car, tSituation *s)
 {
 	closesocket(sock[index]);
+	// TODO: Clear out sock, fds and client
 }
 
 
